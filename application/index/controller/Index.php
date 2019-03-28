816bd43a95f36ddd04cbe5459dd131d6f9a1d591
Domic Chen <2283739236@qq.com>
2019-03-28 22:33:04 +0800

    主页代码优化

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -16,8 +16,7 @@ class Index extends Controller
         // hidden用field代替获取users的数据 model的预加载中有回调则 model调用field会出问题
         // 外键为空的问题
         $data = (new WordsService)->list();
-
-        
+           
         $data and $this->assign('data',$data);
         return $this->fetch();
     }
